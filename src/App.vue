b9e8fb4ee81f549664cedd56eb13c7d2757b0e2b
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2024-04-03 15:02:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Footer from '@/components/Footer.vue'
+
+</script>
+
+<template>
+  <div id="layout">
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div class="wrapper">
+      <nav>
+        <RouterLink :to="{ name: 'event-list'}">Events</RouterLink> |
+        <RouterLink :to="{ name: 'about'}">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+  <Footer />
+</div>
+</template>
+
+<style >
+#layout {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+h2 {
+  font-size: 20px;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <div class="footer-container">
+      <p>Built by <a href="http://akickia.se" target="_blank">@akickia</a> trough <a href="https://vuemastery.com" target="_blank">VueMastery</a></p>
+    </div>
+  </footer>
+</template>
+
+<style>
+footer {
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+}
+.footer-container {
+  text-align: center;
+}
+</style>
